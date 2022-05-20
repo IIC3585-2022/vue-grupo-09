ab39c790fa9374c9b0973904de8ec63cb685fc4d
julio <jnandrade@uc.cl>
2022-05-20 16:26:55 -0400

    Some style

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -28,4 +28,16 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.weather-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  background-color: hsla(154, 73%, 68%, 0.2);
+  font-size: auto;
+  color: white;
+}
+</style>
